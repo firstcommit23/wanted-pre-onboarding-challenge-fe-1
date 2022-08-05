f98feae47310fa31469a85aad0f846c1b101b1a8
jimin <jm131313.kim@gmail.com>
2022-08-06 00:57:36 +0900

    feat: tailwind setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Header from '@/components/Header';
+import '../main.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
